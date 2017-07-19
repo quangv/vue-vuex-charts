671c17e17a9c4a1a7d63a1ec779c5891a01b3dda
Quang Van <quang@toptal.com>
2017-07-19 15:22:05 -0400

    Day componenent bootstrapped

--- src/components/Day.vue
+++ src/components/Day.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="day">
-    Day {{ name }}
+    <input type="number" :placeholder="name" min="0" />
+    <button class="btn btn-success"><i class="glyphicon glyphicon-plus"></i></button>
+    <button class="btn btn-danger"><i class="glyphicon glyphicon-minus"></i></button>
   </div>
 </template>
+
+<style scoped>
+.day {
+  padding: 0.25em 0;
+}
+</style>
 
 <script>
 const names = {
